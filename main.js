c0b6599b5605f6b8ec0f3c0b712c0d35c08ad2d0
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-11-05 19:44:02 -0700

    Added 'The Team' page which pulls data from JSON file via AngularJS

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var app = angular.module('catalog', []);
 var itemData;
 
 function listCtrl($scope, $http) {
-	$http({method:'GET', url: 'items.json'}).success(function(data) {
+	$http({method:'GET', url: '/items.json'}).success(function(data) {
 		$scope.items = data;
 		console.log($scope.items);
 	});
 }
 
 function teamCtrl($scope, $http) {
-	$http({method: 'GET', url: 'team.json'}).success(function(data) {
+	$http({method: 'GET', url: '/team.json'}).success(function(data) {
 		$scope.team = data;
 		console.log($scope.team);
 	});
